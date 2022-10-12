6568c0a7a1955736cb2076405a32a51323a34eb1
Kevinoo5 <kevin.noormets@voco.ee>
2022-10-12 10:49:07 +0300

    As a user I can add book to book table [delivers #183481349]

---
+++ app.js
@@ -0,0 +1,27 @@
+const formSubmit = document.querySelector("#submit-book")
+const inputTitle = document.querySelector("#title")
+const inputAuthor = document.querySelector("#author")
+const inputISBN = document.querySelector("#isbn")
+const table = document.querySelector("table")
+
+
+formSubmit.addEventListener("click", addBook)
+
+
+function addBook(){
+    let newRow = table.insertRow()
+    for (let i = 0; i < 4; i++){
+        const inputs = [inputTitle.value, inputAuthor.value, inputISBN.value]
+        let td = document.createElement("td")
+        td.appendChild(document.createTextNode(inputs[i]))
+        if (i === 3) {
+            const a = document.createElement('a')
+            td = document.createElement("td")
+            a.appendChild(document.createTextNode('X'))
+            a.className = 'blue-text text-darken-2'
+            a.setAttribute('href', '#')
+            td.appendChild(a)
+        }
+        newRow.appendChild(td)
+    }
+}
